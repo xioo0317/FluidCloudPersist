d86dcbff7e9a4826f5f60e5614bc434c0d186888
xioo0317 <2977401730@qq.com>
2026-09-24 18:03:27 +0800

    Initial commit: FluidCloud Persist Xposed module

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.vefaas.fluidcloudpersist"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vefaas.fluidcloudpersist"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    lint {
+        abortOnError = false
+    }
+}
+
+dependencies {
+    compileOnly(fileTree("libs") { include("*.jar") })
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
+}
