90117e86df8769965fd546bed4ed3b7666c0d830
xioo0317 <2977401730@qq.com>
2026-09-25 17:01:13 +0800

    fix: use local Xposed API jar instead of Maven dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 }
 
 dependencies {
-    compileOnly("de.robv.android.xposed:api:82")
+    compileOnly(files("libs/XposedBridgeAPI-82.jar"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
